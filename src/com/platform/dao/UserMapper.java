d7f67f5a1cb1355f90c247427e5c184199520d1b
aiadaniel <lxm445460736@gmail.com>
2017-05-27 18:10:35 +0800

    spring-mybatis config; mybatis-generator config ;

---
+++ src/com/platform/dao/UserMapper.java
@@ -0,0 +1,17 @@
+package com.platform.dao;
+
+import com.platform.entity.User;
+
+public interface UserMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(User record);
+
+    int insertSelective(User record);
+
+    User selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(User record);
+
+    int updateByPrimaryKey(User record);
+}
